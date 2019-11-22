290e14004ae0ef85465a27101bf28f56e366db6d
saintnamee <saintnamee@hotmail.com>
2019-11-22 19:08:58 +0700

    'edit'

--- calculator.test.js
+++ calculator.test.js
@@ -1,10 +1,10 @@
 const calculator = require('./calculator')
 
 describe('add function', () => {
-    test('should return 4 if a = 2 and b = 2', () => {
-        const a = 2;
+    test('should return 3 if a = 1 and b = 2', () => {
+        const a = 1;
         const b = 2;
-        const expected = 4;
+        const expected = 3;
         const result = calculator.add(a, b);
         expect(result).toBe(expected)
     })
